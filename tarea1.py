ede8cda95ff65045028c189e8580804d7f60d00e
juan <jjleoncamilo@gmail.com>
2019-04-12 10:57:19 -0500

    inclusión de funciones en una clase

--- tarea1.py
+++ tarea1.py
@@ -1,4 +1,5 @@
 import csv
+import math
 import pandas as pd  
 import numpy as np  
 import matplotlib.pyplot as plt  
@@ -21,7 +22,7 @@ def Pearson(x,y):
         sum3+=y[i]
         sum4+=x[i]**2
         sum5+=y[i]**2
-    return (sum1-((sum2*sum3)/n))/(sqrt(sum4-((sum2**2)/n))*sqrt(sum5-((sum3**2)/n)))
+    return (sum1-((sum2*sum3)/n))/(math.sqrt(sum4-((sum2**2)/n))*math.sqrt(sum5-((sum3**2)/n)))
 def Coseno(x,y):
     if(len(x)!=len(y)):
         print("error de tamaño")
@@ -34,33 +35,39 @@ def Coseno(x,y):
         sum1+=x[i]**2
         sum2+=y[i]**2
         sum3+=x[i]*y[i]
-    return sum3/(sqrt(sum1)*sqrt(sum2))
-
-documento = open("Movie_ratings.csv", "r")
-####Preprocesamiento
-matriz=[]
-for x in documento:
-    x=x.rstrip('\n')
-    matriz.append(x.split(","))
-nombres=matriz[0]
-peliculas=[]
-matriz=matriz[1:]
-for i in range (len(matriz)):
-    peliculas.append(matriz[i][0])
-    matriz[i]=matriz[i][1:]
-    for ix in range(len(matriz[i])):
-        if(matriz[i][ix]==''):
-            matriz[i][ix]=0
-        else:
-            matriz[i][ix]=int(matriz[i][ix])
-matriz_t=[[matriz[j][i] for j in range(len(matriz))] for i in range(len(matriz[0]))]
+    return sum3/(math.sqrt(sum1)*math.sqrt(sum2))
+class SistemaDeRecomendacion:
+    def __init__(self):
+        self.matriz=[]
+        self.peliculas=[]
+        self.preprocesamiento()
+    def preprocesamiento(self,archivo="Movie_ratings.csv"):
+        documento = open("Movie_ratings.csv", "r")
+        ####Preprocesamiento
+        matriz=[]
+        for x in documento:
+            x=x.rstrip('\n')
+            matriz.append(x.split(","))
+        self.nombres=matriz[0]
+        peliculas=[]
+        matriz=matriz[1:]
+        for i in range (len(matriz)):
+            peliculas.append(matriz[i][0])
+            matriz[i]=matriz[i][1:]
+            for ix in range(len(matriz[i])):
+                if(matriz[i][ix]==''):
+                    matriz[i][ix]=0
+                else:
+                    matriz[i][ix]=int(matriz[i][ix])
+        self.matriz_t=[[matriz[j][i] for j in range(len(matriz))] for i in range(len(matriz[0]))]
+    def imprimir(self):
+        print("Distancia entre dos: ",Pearson(self.matriz_t[0],self.matriz_t[1]))
+        print("Distancia entre dos: ",Coseno(self.matriz_t[0],self.matriz_t[1]))
+    def recommendar(self,nombre,pelicula):
+        print("here")
 ##################
-print(matriz)
-print("")
-print(matriz_t)
-transpuesta=[[matriz[j][i] for j in range(len(matriz))] for i in range(len(matriz[0]))]
-print(peliculas)
-print(nombres)
+miSistema=SistemaDeRecomendacion()
+miSistema.imprimir()
 """
 for i in range(len(matriz)):
     matriz[i]=matriz[1:]
